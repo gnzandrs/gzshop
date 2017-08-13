90d8a8953479ad3a07efcf463b457486543ef2f1
gnzandrs <gavr@outlook.cl>
2017-08-13 18:00:23 -0300

    filters in catalog

--- js/app.js
+++ js/app.js
@@ -39,3 +39,19 @@ function mostrarModal() {
 		localStorage.noMostrarModal = true;
 	});
 }
+
+const $filtrosToggle = $('#filtrosToggle');
+
+$filtrosToggle.on('click', function (e) {
+	e.preventDefault();
+
+	const $i = $filtrosToggle.find('i.fa');
+	const isDown = $i.hasClass('fa-chevron-down')
+
+	if (isDown) {
+		$i.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+	} else {
+		$i.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+	}
+
+})
